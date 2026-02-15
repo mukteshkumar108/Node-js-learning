0b5e06a3e8d58c0a621f19911c047fa52fba8d7d
Muktesh kumar <mukteshkumar108@gmail.com>
2026-02-15 23:56:48 +0530

    Add divide() with error handling

--- Hello World/math.js
+++ Hello World/math.js
@@ -11,4 +11,12 @@ function multiply(a, b) {
   return a * b;
 }
 
-module.exports = {add, subtract, multiply};
+function divide(a, b){
+    if(b === 0){
+        throw new Error("Division by zero is not allowed");
+    }
+    return a / b;
+}
+
+
+module.exports = {add, subtract, multiply, divide};
