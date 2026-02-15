d9ae9111a795f6c6d134fdbeadbd46e0fae68bd6
Muktesh kumar <mukteshkumar108@gmail.com>
2026-02-15 23:51:24 +0530

    integrate math module into hello script

--- Hello World/hello.js
+++ Hello World/hello.js
@@ -1,1 +1,6 @@
-console.log("Hello Muktesh, All the best for Self taught journey");
+// console.log("Hello Muktesh, All the best for Self taught journey");
+
+const math = require('./math')
+
+console.log("Hello, Node.js 🚀");
+console.log("2 + 3 =", add(2, 3));
