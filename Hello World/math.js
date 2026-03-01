39d55d9cf43fb160143085f2e12047365fdb6b2d
Muktesh kumar <mukteshkumar108@gmail.com>
2026-03-01 23:55:18 +0530

    add internal logging utility to math operations

--- Hello World/math.js
+++ Hello World/math.js
@@ -1,31 +1,47 @@
+// Helper function to log activities (Internal use only)
+const logAction = (name, result) => {
+  const timestamp = new Date().toLocaleTimeString();
+  console.log(`[${timestamp}] Operation: ${name} | Result: ${result}`);
+};
 
-function add(a , b){
-    return a + b
+function add(a, b) {
+  const res = a + b;
+  logAction('Add', res);
+  return res;
 }
 
 function subtract(a, b) {
-  return a - b;
+  const res = a - b;
+  logAction('Subtract', res);
+  return res;
 }
 
 function multiply(a, b) {
-  return a * b;
+  const res = a * b;
+  logAction('Multiply', res);
+  return res;
 }
 
-function divide(a, b){
-    if(b === 0){
-        throw new Error("Division by zero is not allowed");
-    }
-    return a / b;
+function divide(a, b) {
+  if (b === 0) {
+    console.error("Attempted to divide by zero!");
+    throw new Error("Division by zero is not allowed");
+  }
+  const res = a / b;
+  logAction('Divide', res);
+  return res;
 }
 
-// function sum(...nums){
-//   return nums.reduce((accumulator, current) => accumulator + current, 0);
-// }
+function sum(...nums) {
+  const res = nums.reduce((acc, curr) => acc + curr, 0);
+  logAction('Sum Total', res);
+  return res;
+}
 
-
-module.exports = {add, 
-                  subtract, 
-                  multiply, 
-                  divide, 
-                  // sum
-                };
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  sum,
+};
